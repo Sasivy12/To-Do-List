a6cf7991278dbc20f0527fbe3040bec255f0808e
Sasivy12 <113285743+Sasivy12@users.noreply.github.com>
2023-11-05 17:57:22 +0300

    Create App.cpp
    App initialization

---
+++ App.cpp
@@ -0,0 +1,16 @@
+#include "App.h"
+#include "MainFrame.h"
+#include <wx/wx.h>
+
+wxIMPLEMENT_APP(App);
+
+bool App::OnInit()
+{
+	MainFrame* mainframe = new MainFrame("TO-DO List");
+	mainframe->SetClientSize(800, 600);
+	mainframe->Center();
+
+	mainframe->Show();
+
+	return true;
+}
